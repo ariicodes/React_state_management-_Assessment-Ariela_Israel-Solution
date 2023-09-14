9c791ef16f2e4a6ed56051459b438c387bf0ba62
Ariela <arielaisrael12@gmail.com>
2023-09-14 02:35:09 -0400

    the ability to create new posts added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import PostCreate from "./PostCreate";
-import PostList from "./PostList";
+import React, { useState } from 'react';
+import './App.css';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 function App() {
-  const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState([]);
 
-  // TODO: Add the ability for the <PostCreate /> component to create new posts.
-  // TODO: Add the ability for the <PostList /> component to delete an existing post.
+	// TODO: Add the ability for the <PostCreate /> component to create new posts.
+	// TODO: Add the ability for the <PostList /> component to delete an existing post.
 
-  return (
-    <div className="App">
-      <PostCreate />
-      <PostList />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<PostCreate
+				posts={posts}
+				setPosts={setPosts}
+			/>
+			<PostList />
+		</div>
+	);
 }
 
 export default App;
