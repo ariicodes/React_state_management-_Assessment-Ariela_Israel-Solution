193e9daac1d4c083202c96e8a693712065d0fd5b
Ariela <arielaisrael12@gmail.com>
2023-09-14 03:18:26 -0400

    posts can be displayed

--- src/PostList.js
+++ src/PostList.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React from 'react';
+import Post from './Post';
 
 /**
  * Diplays a list of either text or image posts.
  *
  */
-function PostList() {
-  // TODO: Diplay the list of posts.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each post must have className="post" for the tests to work.
-  // TODO: Each post must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+function PostList({ posts, onDelete }) {
+	// TODO: Diplay the list of posts.
+	// TODO: Create at least one additional component that is used by this component.
+	// TODO: Each post must have className="post" for the tests to work.
+	// TODO: Each post must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
 
-  return <div className="post-list"></div>;
+	const list = posts.map((post, i) => (
+		<li key={i}>
+			<Post
+				type={post.type}
+				content={post.content}
+				deleteBtnHandler={() => onDelete(i)}
+			/>
+		</li>
+	));
+
+	return (
+		<div className='post-list'>
+			<ul>{list}</ul>
+		</div>
+	);
 }
 
 export default PostList;
